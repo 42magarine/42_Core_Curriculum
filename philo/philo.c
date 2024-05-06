737b9a8f0526cdf0b07ac4420341855f70faa592
Mathias Ott <mott@student.42heilbronn.de>
2024-05-06 19:52:31 +0200

    try some stuff from code vault

--- philo/philo.c
+++ philo/philo.c
@@ -6,79 +6,83 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 17:20:36 by mott              #+#    #+#             */
-/*   Updated: 2024/04/25 19:15:47 by mott             ###   ########.fr       */
+/*   Updated: 2024/05/06 19:05:32 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+pthread_mutex_t	g_mutex;
+pthread_mutex_t	g_mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
 void	ft_usleep(void)
 {
 	int	usec;
-	int	i;
 
 	usec = 999999;
-	i = usleep(usec);
-	printf("usleep: %i\n", i);
-}
-
-void	*ft_usleep2(void *arg)
-{
-	int	usec;
-	int	i;
-
-	usec = *(int *)arg;
-	i = usleep(usec);
-	printf("usleep2: %i\n", i);
-	return (NULL);
+	usleep(usec);
 }
 
 void	ft_gettimeofday(void)
 {
 	struct timeval	tv;
-	int				i;
 
-	i = gettimeofday(&tv, NULL);
-	printf("i: %d\n", i);
+	gettimeofday(&tv, NULL);
 	printf("tv_sec: %ld\n", tv.tv_sec);		// seconds since 01.01.1970 00:00:00
 	printf("tv_usec: %d\n", tv.tv_usec);	// microseconds within the current second (1.000.000 usec = 1 sec)
 }
 
-pthread_t	ft_pthread_create(void)
+void	*start_routine(void *id)
 {
 	pthread_t	tid;
-	int			usec;
+	int			*i;
 
-	usec = 999999;
-	pthread_create(&tid, NULL, ft_usleep2, (void *)&usec);
-	printf("pthread_create: %li\n", tid->__sig);
-	return (tid);
-}
+	tid = (pthread_t)id;
+	printf("start pthread: %ld\n", tid->__sig);
 
-void	ft_pthread_detach(pthread_t tid)
-{
-	int	i;
+	i = malloc(sizeof(int));
+	*i = 0;
+	while ((*i)++ < 1000000)
+	{
+		pthread_mutex_lock(&g_mutex);
+		// n++;
+		pthread_mutex_unlock(&g_mutex);
+	}
 
-	i = pthread_detach(tid);
-	printf("pthread_detach: %i\n", i);
-}
-
-void	ft_pthread_join(pthread_t tid)
-{
-	int	i;
-
-	i = pthread_join(tid, NULL);
-	printf("pthread_join: %i\n", i);
+	printf("end pthread: %ld\n", tid->__sig);
+	return ((void *)i);
 }
 
 int	main(void)
 {
-	pthread_t	tid;
+	pthread_t	tid[8];
+	int			*result[8];
+	int			i;
 
-	// ft_usleep();
-	// ft_gettimeofday();
-	tid = ft_pthread_create();
-	ft_pthread_detach(tid);
-	// ft_pthread_join(tid);
+	pthread_mutex_init(&g_mutex, NULL);
+
+	i = 0;
+	while (i++ < 8)
+	{
+		if (pthread_create(&tid[i], NULL, start_routine, &tid[i]) != 0)
+			return (1);
+	}
+
+	pthread_mutex_destroy(&g_mutex);
+
+	i = 0;
+	while (i++ < 8)
+	{
+		if (pthread_join(tid[i], (void **)&result[i]) != 0)
+			return (2);
+	}
+
+	i = 0;
+	while (i++ < 8)
+	{
+		printf("i: %d result: %d\n", i, *result[i]);
+		free(result[i]);
+	}
+
 	return (EXIT_SUCCESS);
 }
