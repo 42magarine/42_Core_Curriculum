c24ed16b78e54458b9e573820b5a06adaa6543eb
Mathias Ott <mott@student.42heilbronn.de>
2024-07-10 15:50:20 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,14 +6,24 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 14:07:54 by mott              #+#    #+#             */
-/*   Updated: 2024/07/10 14:39:11 by mott             ###   ########.fr       */
+/*   Updated: 2024/07/10 15:49:16 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int	main()
-{
-	std::cout << "Hello World!" << std::endl;
+int main(int argc, char **argv) {
+	if (argc > 1) {
+		for (int i = 1; i < argc; i++) {
+			for (int j = 0; argv[i][j] != '\0'; j++) {
+				std::cout << (char)toupper(argv[i][j]);
+			}
+		}
+	}
+	else {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	std::cout << std::endl;
 	return 0;
 }
