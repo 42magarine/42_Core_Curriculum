98a385e8d286892f43e97de289cc456efa5a41b9
Mathias Ott <mott@student.42heilbronn.de>
2024-07-13 14:24:43 +0200

    constructor + destructor + getFunctions

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,68 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 12:52:33 by mott              #+#    #+#             */
-/*   Updated: 2024/07/13 12:52:34 by mott             ###   ########.fr       */
+/*   Updated: 2024/07/13 14:20:16 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) : _amount(initial_deposit) {
+	_accountIndex = Account::getNbAccounts();
+	_nbAccounts++;
+	_totalAmount += _amount;
+	std::cout << "index:" << _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "created" << std::endl;
+}
+
+Account::~Account() {
+	std::cout << "index:" << _accountIndex << ";"
+			  << "amount:" << _amount << ";"
+			  << "closed" << std::endl;
+}
+
+int	Account::getNbAccounts() {
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount() {
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits() {
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals() {
+	return _totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos() {
+
+}
+
+void	Account::makeDeposit(int deposit) {
+	_amount += deposit;
+	_nbDeposits++;
+}
+
+bool	Account::makeWithdrawal(int withdrawal) {
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+	return true;
+}
+
+int	Account::checkAmount() const {
+	return 0;
+}
+
+void	Account::displayStatus() const {
+
+}
