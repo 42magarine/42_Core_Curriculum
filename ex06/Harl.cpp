3e042ebab3913f4b71be17eb69015df8283cea63
Mathias Ott <mott@student.42heilbronn.de>
2024-07-19 14:34:46 +0200

    ex06 with switch statement

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 18:49:52 by mott              #+#    #+#             */
-/*   Updated: 2024/07/18 18:49:54 by mott             ###   ########.fr       */
+/*   Updated: 2024/07/19 14:31:41 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,49 @@ Harl::Harl() {
 Harl::~Harl() {}
 
 void Harl::debug() {
-	std::cout << "I love having extra bacon for my"
-				 " 7XL-double-cheese-triple-pickle-special-ketchup burger."
-				 " I really do!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl
+			  << "I love having extra bacon for my"
+			  << " 7XL-double-cheese-triple-pickle-special-ketchup burger."
+			  << " I really do!" << std::endl;
 }
 
 void Harl::info() {
-	std::cout << "I cannot believe adding extra bacon costs more money."
-				 " You didn't put enough bacon in my burger!"
-				 " If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl
+			  << "I cannot believe adding extra bacon costs more money."
+			  << " You didn't put enough bacon in my burger!"
+			  << " If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void Harl::warning() {
-	std::cout << "I think I deserve to have some extra bacon for free."
-				 " I've been coming for years whereas you started working"
-				 " here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl
+			  << "I think I deserve to have some extra bacon for free."
+			  << " I've been coming for years whereas you started working"
+			  << " here since last month." << std::endl;
 }
 
 void Harl::error() {
-	std::cout << "This is unacceptable!"
-				 " I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl
+			  << "This is unacceptable!"
+			  << " I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level) {
-	for (int i = 0; i < 4; ++i) {
-		if (level == _level[i]) {
-			(this->*function[i])();
-			return;
-		}
+	int i = 0;
+
+	while (i < 4 && level != _level[i]) {
+		i++;
+	}
+	switch (i) {
+		case 0:
+			(this->*function[0])();
+		case 1:
+			(this->*function[1])();
+		case 2:
+			(this->*function[2])();
+		case 3:
+			(this->*function[3])();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 }
--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,19 +6,20 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 18:49:24 by mott              #+#    #+#             */
-/*   Updated: 2024/07/18 18:49:30 by mott             ###   ########.fr       */
+/*   Updated: 2024/07/19 13:59:42 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Harl.hpp"
 
-int main() {
+int main(int argc, char **argv) {
 	Harl Harl;
 
-	Harl.complain("DEBUG");
-	Harl.complain("INFO");
-	Harl.complain("WARNING");
-	Harl.complain("ERROR");
-
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <Filter>" << std::endl;
+		return 1;
+	}
+	Harl.complain(argv[1]);
 	return 0;
 }
