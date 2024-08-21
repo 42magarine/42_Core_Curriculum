40c474c0fcf274f536d81ee50dfa0f9ea88174b0
mott <mott@student.42heilbronn.de>
2024-08-21 19:05:05 +0200

    add FragTrap class

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,28 +6,42 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 18:07:33 by mott              #+#    #+#             */
-/*   Updated: 2024/08/21 18:07:34 by mott             ###   ########.fr       */
+/*   Updated: 2024/08/21 18:57:23 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
-#include <iostream>
 
-FragTrap::FragTrap() {
-	std::cout << "Default constructor called" << std::endl;
+FragTrap::FragTrap() : ClapTrap() {
+	_hit_points = 100;
+	_energy_points = 100;
+	_attack_damage = 30;
+	std::cout << "FragTrap " << _name << " has been default-constructed." << std::endl;
 }
 
-FragTrap::FragTrap(const FragTrap &other) {
-	std::cout << "Copy constructor called" << std::endl;
+FragTrap::FragTrap(std::string name) : ClapTrap(name) {
+	_hit_points = 100;
+	_energy_points = 100;
+	_attack_damage = 30;
+	std::cout << "FragTrap " << _name << " has been constructed." << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &other) : ClapTrap(other) {
+	std::cout << "FragTrap " << _name << " has been copy-constructed." << std::endl;
 }
 
 FragTrap::~FragTrap() {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "FragTrap " << _name << " has been destroyed." << std::endl;
 }
 
 FragTrap& FragTrap::operator=(const FragTrap &other) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other) {
+		ClapTrap::operator=(other);
 	}
+	std::cout << "FragTrap " << _name << " has been copy-assigned." << std::endl;
 	return *this;
+}
+
+void FragTrap::highFivesGuys() {
+	std::cout << "FragTrap " << _name << " is requesting a high five!" << std::endl;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 18:05:52 by mott              #+#    #+#             */
-/*   Updated: 2024/08/21 18:06:20 by mott             ###   ########.fr       */
+/*   Updated: 2024/08/21 19:03:35 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 #include "FragTrap.hpp"
 
 int main() {
+	FragTrap ft_default;
+	FragTrap ft_name("frag with a name");
+	FragTrap ft_copy(ft_name);
+	FragTrap ft_assignment;
+	ft_assignment = ft_default;
+	std::cout << std::endl;
+
+	ft_name.attack("target1");
+	ft_name.highFivesGuys();
+	std::cout << std::endl;
+
 	return 0;
 }
