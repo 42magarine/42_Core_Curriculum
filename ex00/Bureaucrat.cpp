7113ee4067e791e525220f9bf23a0437f034db7d
mott <mott@student.42heilbronn.de>
2024-09-23 15:14:54 +0200

    basis implementation of Bureaucrat class

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,10 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 12:39:24 by mott              #+#    #+#             */
+/*   Updated: 2024/09/23 13:49:24 by mott             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() {
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150) {
 	std::cout << YELLOW << "Default constructor called" << RESET << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& other) {
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name), _grade(grade) {
+	std::cout << YELLOW << "Name constructor called" << RESET << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {
 	std::cout << YELLOW << "Copy constructor called" << RESET << std::endl;
 }
 
@@ -14,7 +30,29 @@ Bureaucrat::~Bureaucrat() {
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
 	if (this != &other) {
+		_grade = other._grade;
 		std::cout << YELLOW << "Copy assignment operator called" << RESET << std::endl;
 	}
 	return *this;
+}
+
+std::string Bureaucrat::getName() const {
+	return _name;
+}
+
+unsigned int Bureaucrat::getGrade() const {
+	return _grade;
+}
+
+void Bureaucrat::incrementGrade() {
+	_grade--;
+}
+
+void Bureaucrat::decrementGrade() {
+	_grade++;
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
+	return os;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,10 +6,34 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 12:30:27 by mott              #+#    #+#             */
-/*   Updated: 2024/09/23 12:30:42 by mott             ###   ########.fr       */
+/*   Updated: 2024/09/23 13:51:25 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
 int main() {
+	Bureaucrat a;
+	Bureaucrat b("b", 1);
+	Bureaucrat c(b);
+	Bureaucrat d;
+	d = a;
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << std::endl;
+
+	a.incrementGrade();
+	b.incrementGrade();
+	c.decrementGrade();
+	d.decrementGrade();
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+
 	return 0;
 }
