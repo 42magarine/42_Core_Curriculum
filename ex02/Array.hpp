038193adb8be43f44832e2e50973122b1bb969d3
mott <mott@student.42heilbronn.de>
2024-09-30 13:36:10 +0200

    change class Array to class template Array + test cases in main()

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:08:56 by mott              #+#    #+#             */
-/*   Updated: 2024/09/30 12:39:44 by mott             ###   ########.fr       */
+/*   Updated: 2024/09/30 13:34:48 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,59 @@
 #define ARRAY_H
 
 #include <iostream>
+#include <string>
 #include <stdexcept>		// std::out_of_range
 
+template <typename T>
 class Array {
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(const Array& other);
+		Array() : _array(nullptr), _size(0) {}
 
-		~Array();
+		Array(unsigned int n) : _array(new T[n]()), _size(n) {}
 
-		Array& operator=(const Array& other);
-		int& operator[](unsigned int index);
-		const int& operator[](unsigned int index) const;
+		Array(const Array& other) : _array(new T[other._size]), _size(other._size) {
+			for (unsigned int i = 0; i < _size; i++) {
+				_array[i] = other._array[i];
+			}
+		}
 
-		unsigned int size() const;
+		~Array() {
+			delete[] _array;
+		}
+
+		Array& operator=(const Array& other) {
+			if (this != &other) {
+				delete[] _array;
+
+				_size = other._size;
+				_array = new T[_size];
+				for (unsigned int i = 0; i < _size; i++) {
+					_array[i] = other._array[i];
+				}
+			}
+			return *this;
+		}
+
+		T& operator[](unsigned int index) {
+			if (index >= _size) {
+				throw std::out_of_range("Index is out of bounds");
+			}
+			return _array[index];
+		}
+
+		const T& operator[](unsigned int index) const {
+			if (index >= _size) {
+				throw std::out_of_range("Index is out of bounds");
+			}
+			return _array[index];
+		}
+
+		unsigned int size() const {
+			return _size;
+		}
 
 	private:
-		int* _array;
+		T* _array;
 		unsigned int _size;
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,47 +6,46 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 18:52:48 by mott              #+#    #+#             */
-/*   Updated: 2024/09/30 12:15:53 by mott             ###   ########.fr       */
+/*   Updated: 2024/09/30 13:34:13 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-int main() {
-	// Array int_array;
-	Array int_array(10);
-	Array int_array_two(int_array);
-	Array int_array_three(int_array);
+// int main() {
+// 	Array<int> int_array(10);
+// 	Array<int> int_array_two(int_array);
+// 	Array<int> int_array_three(int_array);
 
-	for (unsigned int i = 0; i < int_array.size(); i++) {
-		int_array[i] = i;
-	}
-	for (unsigned int i = 0; i < int_array_two.size(); i++) {
-		int_array_two[i] = i * 2;
-	}
-	int_array_three = int_array;
+// 	for (unsigned int i = 0; i < int_array.size(); i++) {
+// 		int_array[i] = i;
+// 	}
+// 	for (unsigned int i = 0; i < int_array_two.size(); i++) {
+// 		int_array_two[i] = i * 2;
+// 	}
+// 	int_array_three = int_array;
 
-	std::cout << "int_array.size(): " << int_array.size() << std::endl;
-	for (unsigned int i = 0; i < int_array.size(); i++) {
-		std::cout << int_array[i] << " ";
-	}
-	std::cout << std::endl;
+// 	std::cout << "int_array.size(): " << int_array.size() << std::endl;
+// 	for (unsigned int i = 0; i < int_array.size(); i++) {
+// 		std::cout << int_array[i] << " ";
+// 	}
+// 	std::cout << std::endl;
 
-	std::cout << "int_array_two.size(): " << int_array_two.size() << std::endl;
-	for (unsigned int i = 0; i < int_array_two.size(); i++) {
-		std::cout << int_array_two[i] << " ";
-	}
-	std::cout << std::endl;
+// 	std::cout << "int_array_two.size(): " << int_array_two.size() << std::endl;
+// 	for (unsigned int i = 0; i < int_array_two.size(); i++) {
+// 		std::cout << int_array_two[i] << " ";
+// 	}
+// 	std::cout << std::endl;
 
-	std::cout << "int_array_three.size(): " << int_array_three.size() << std::endl;
-	for (unsigned int i = 0; i < int_array_three.size(); i++) {
-		std::cout << int_array_three[i] << " ";
-	}
-	std::cout << std::endl;
+// 	std::cout << "int_array_three.size(): " << int_array_three.size() << std::endl;
+// 	for (unsigned int i = 0; i < int_array_three.size(); i++) {
+// 		std::cout << int_array_three[i] << " ";
+// 	}
+// 	std::cout << std::endl;
 
-	system("leaks templates");
-	return 0;
-}
+// 	system("leaks templates");
+// 	return 0;
+// }
 
 // int main() {
 // 	int* a = new int();
@@ -55,3 +54,45 @@ int main() {
 
 // 	return 0;
 // }
+
+// int main() {
+// 	Array<char> array_one(26);
+
+// 	for (unsigned int i = 0; i < array_one.size(); i++) {
+// 		array_one[i] = 'a' + i;
+// 	}
+// 	for (unsigned int i = 0; i < array_one.size(); i++) {
+// 		std::cout << array_one[i] << " ";
+// 	}
+// 	std::cout << std::endl;
+
+// 	try {
+// 		std::cout << array_one[100] << std::endl;
+// 	}
+// 	catch (std::exception& e) {
+// 		std::cout << e.what() << std::endl;
+// 	}
+
+// 	return 0;
+// }
+
+int main() {
+	Array<std::string> array_one(8);
+
+	for (unsigned int i = 0; i < array_one.size(); i++) {
+		array_one[i] = "string_" + std::to_string(i);
+	}
+	for (unsigned int i = 0; i < array_one.size(); i++) {
+		std::cout << array_one[i] << " ";
+	}
+	std::cout << std::endl;
+
+	try {
+		std::cout << array_one[100] << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
+}
