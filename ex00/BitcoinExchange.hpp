c55a922e7099a7ae45bd1d6b6c479dce4d4c3e68
mott <mott@student.42heilbronn.de>
2024-10-20 16:13:00 +0200

    some ideas about ex00

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 15:28:59 by mott              #+#    #+#             */
+/*   Updated: 2024/10/20 16:01:56 by mott             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BITCOINEXCHANGE_H
 #define BITCOINEXCHANGE_H
 
 #include <iostream>
+#include <map>
 
 #define RESET  "\033[0m"
 #define YELLOW "\033[33m"
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,12 +6,24 @@
 /*   By: mott <mott@student.42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 19:24:47 by mott              #+#    #+#             */
-/*   Updated: 2024/10/19 19:28:17 by mott             ###   ########.fr       */
+/*   Updated: 2024/10/20 16:06:43 by mott             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main() {
+int main(int argc, char** argv) {
+
+	// -> valid input file? argc != 2
+
+	// -> open data.csv
+	// -> read data to std::map
+	// -> close data.csv
+
+	// -> class Bitcoin(map, argv[1]);
+	// 	-> read argv line by line
+	// 	-> error check format
+	// 	-> print key/value
+
 	return 0;
 }
