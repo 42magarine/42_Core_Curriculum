de07e875fbf4983e43b9889f2d818070543be677
mott <mott@student.42heilbronn.de>
2025-03-03 18:23:38 +0100

    docker secrets

--- srcs/requirements/mariadb/tools/mariadb-setup.sh
+++ srcs/requirements/mariadb/tools/mariadb-setup.sh
@@ -2,6 +2,9 @@
 
 mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld
 
+DB_PASSWORD=$(cat /run/secrets/db_password | tr -d '\n')
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password | tr -d '\n')
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
 
@@ -12,7 +15,7 @@ DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 CREATE DATABASE ${DB_NAME};
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
--- srcs/requirements/wordpress/tools/wordpress-setup.sh
+++ srcs/requirements/wordpress/tools/wordpress-setup.sh
@@ -1,34 +1,38 @@
 #!/bin/sh
 
+DB_PASSWORD=$(cat /run/secrets/db_password | tr -d '\n')
+WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password | tr -d '\n')
+WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password | tr -d '\n')
+
 if [ ! -f /var/www/html/wp-config.php ]; then
     # This downloads the WordPress core files.
     wp core download --allow-root --path=/var/www/html --version=6.7.1
 
     # This will generate the WordPress configuration file (wp-config.php).
     wp config create --allow-root \
-        --dbname="${DB_NAME}" \
-        --dbuser="${DB_USER}" \
-        --dbpass="${DB_USER_PASSWORD}" \
-        --dbhost="${DB_HOST}"
+        --dbname="$DB_NAME" \
+        --dbuser="$DB_USER" \
+        --dbpass="$DB_PASSWORD" \
+        --dbhost="$DB_HOST"
 
     # This will then install WordPress.
     wp core install --allow-root \
-        --url="${WP_URL}" \
-        --title="${WP_TITLE}" \
-        --admin_user="${WP_ADMIN_USER}" \
-        --admin_password="${WP_ADMIN_PASSWORD}" \
-        --admin_email="${WP_ADMIN_EMAIL}"
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL"
 
     # This create a new WordPress user.
-    wp user create "${WP_USER_NAME}" "${WP_USER_EMAIL}" \
-        --user_pass="${WP_USER_PASSWORD}" \
-        --role="${WP_USER_ROLE}"
+    wp user create "$WP_USER_NAME" "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role="$WP_USER_ROLE"
 
     wp theme activate twentytwentyfour --allow-root
 
     # Install and configure Redis plugin
-    wp config set WP_REDIS_HOST redis --allow-root
-    wp config set WP_REDIS_PORT 6379 --allow-root
+    wp config set WP_REDIS_HOST "$REDIS_HOST" --allow-root
+    wp config set WP_REDIS_PORT "$REDIS_PORT" --allow-root
     wp config set WP_CACHE true --allow-root --raw
     wp config set WP_REDIS_CLIENT phpredis --allow-root
 
